22fc8074ab78e4891f15e85f002ec412859bef1e
Christoffer Moesgaard <christoffer@magenta.dk>
2020-12-01 17:31:48 +0100

    More day 1 refactorings

--- 1.py
+++ 1.py
@@ -1,23 +1,27 @@
 import math
 from itertools import combinations
 
-with open('input/1', 'r') as f:
-    numbers = list(map(int, f.readlines()))
+
+def get_numbers():
+    with open('input/1', 'r') as f:
+        return map(int, f.readlines())
 
 
-def find_values(number_list, num_of_combinations):
-    combis = combinations(number_list, num_of_combinations)
+def find_products(numbers, num_of_combinations):
+    combis = combinations(numbers, num_of_combinations)
     filtered = filter(lambda x: sum(x) == 2020, combis)
     prods = map(math.prod, filtered)
     return prods
 
 
 def part_one():
-    return find_values(numbers, 2)
+    numbers = get_numbers()
+    return find_products(numbers, 2)
 
 
 def part_two():
-    return find_values(numbers, 3)
+    numbers = get_numbers()
+    return find_products(numbers, 3)
 
 
 print(list(part_one()))
