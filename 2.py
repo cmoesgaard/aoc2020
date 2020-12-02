766bbcea825c0a0625aab37f07e94df9e7477a7a
Christoffer Moesgaard <christoffer@magenta.dk>
2020-12-02 18:15:23 +0100

    Less stupid day 2 attempt

--- 2.py
+++ 2.py
@@ -3,42 +3,37 @@ def get_input():
         return [line.strip() for line in f.readlines()]
 
 
-def filter_passwords(input_list, filter_fn_generator):
-    tuples = [line.split(': ') for line in input_list]
-    mapped = [filter_fn_generator(policy)(password) for policy, password in tuples]
-    return mapped.count(True)
+def parse_line(line):
+    policy, password = line.split(': ')
+    bounds, letter = policy.split(' ')
+    lower, upper = bounds.split('-')
+    return int(lower), int(upper), letter, password
+
+
+def filter_passwords(input_list, filter_fn):
+    parsed_input = map(parse_line, input_list)
+    filtered = filter(filter_fn, parsed_input)
+    return len(list(filtered))
 
 
 def part_one():
     puzzle_input = get_input()
 
-    def generate_filter_fn(policy):
-        bounds, letter = policy.split(' ')
-        bounds_tuple = bounds.split('-')
-        lower_bound, upper_bound = [int(index) for index in bounds_tuple]
+    def filter_fn(password_tuple):
+        lower, upper, letter, password = password_tuple
+        return lower <= password.count(letter) <= upper
 
-        def filter_fn(x):
-            return lower_bound <= x.count(letter) <= upper_bound
-
-        return filter_fn
-
-    return filter_passwords(puzzle_input, generate_filter_fn)
+    return filter_passwords(puzzle_input, filter_fn)
 
 
 def part_two():
     puzzle_input = get_input()
 
-    def generate_filter_fn(policy):
-        indices, letter = policy.split(' ')
-        index_tuple = indices.split('-')
-        first, last = [int(index) - 1 for index in index_tuple]
+    def filter_fn(password_tuple):
+        lower, upper, letter, password = password_tuple
+        return (password[lower - 1] == letter) ^ (password[upper - 1] == letter)
 
-        def filter_fn(x):
-            return (x[first] == letter) ^ (x[last] == letter)
-
-        return filter_fn
-
-    return filter_passwords(puzzle_input, generate_filter_fn)
+    return filter_passwords(puzzle_input, filter_fn)
 
 
 print(part_one())
